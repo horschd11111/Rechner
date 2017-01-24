76ccdad67e527ef5b34d354f902cb878b37b93a0
horschd11111 <kilian.theis@gmx.de>
2017-01-24 15:07:12 +0100

    Grundrechenarten implementiert

---
+++ Rechner/src/SimpleMath.java
@@ -0,0 +1,16 @@
+
+public class SimpleMath {
+
+	public static double add(double x, double y){
+		return x+y;
+	}
+	public static double substract(double x, double y){
+		return x-y;
+	}
+	public static double multiply(double x, double y){
+		return x*y;
+	}
+	public static double divide(double x, double y){
+		return x/y;
+	}
+}
