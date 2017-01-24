a4a7bbb6cbb69a3f9c2d239208ea28ce420a1649
robertoro <r.roberto@t-online.de>
2017-01-24 15:13:19 +0100

    Rechner implementiert

--- Rechner/src/Main.java
+++ Rechner/src/Main.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner einlesen = new Scanner(System.in);
+		
+		double a = einlesen.nextDouble();
+		char c = einlesen.next().charAt(0);
+		double b = einlesen.nextDouble();
+		
+		switch(c) {
+		case '+':	System.out.println("" + SimpleMath.add(a, b));
+					break;
+		case '-': 	System.out.println("" + SimpleMath.substract(a, b));
+					break;
+		case '*':   System.out.println("" + SimpleMath.multiply(a, b));
+					break;
+		case '/': 	System.out.println("" + SimpleMath.divide(a, b));
+					break;
+		default: 	System.out.println("Fehler");
+		}
 
 	}
 
